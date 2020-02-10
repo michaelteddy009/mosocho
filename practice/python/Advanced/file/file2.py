597875499d018f767d33e2b630740b15e7372ef0
michaelteddy009 <michaelteddy009@gmail.com>
2020-02-10 16:20:47 +0300

    Another Change

--- practice/python/Advanced/file/file2.py
+++
@@ -1,4 +0,0 @@
-with open('attachments/file2.py', 'w') as f: #creates the file incase it never existed
-	#f.write('Michael just keep going') #this will write to a file and on repeat will write another linejust like the read while passed with size but if we use f.seek(0) we will overwrite the first write
-	#Not that copy files like pictures wount be possible but we can use mode rb and wb to read and write in binary 		 		 		    		
-
