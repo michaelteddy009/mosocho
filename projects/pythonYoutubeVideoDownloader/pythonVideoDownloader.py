26f9c76f5e3a39445cabed5e48155142abb544f7
michaelteddy009 <michaelteddy009@gmail.com>
2020-02-13 12:38:49 +0300

    anotherCommit

--- projects/pythonYoutubeVideoDownloader/pythonVideoDownloader.py
+++ projects/pythonYoutubeVideoDownloader/pythonVideoDownloader.py
@@ -5,22 +5,23 @@ import os
 ydl_opts = {}
 os.chdir('F:/Downloads')
 
-makeDirectory = input("\nCreate new directory for your downloads [y/n] :")
+makeDirectory = input("\nCreate new directory for your downloads or use an existing one [y/n] :")
 if makeDirectory == 'n':
 	print("\nNo directory made for download so defaults to 'F/downloads\n'")
 elif makeDirectory == 'y':
 	down = input('\nName folder for downloads : ')
 	path = os.path.join(os.getcwd(), down)
 	if os.path.isdir(path):
-		print("Path already exits so will be used for download.")
+		print('Path folder already exits\n')
 		print('Downloading to {}\n'.format(path))
 	else:
 		os.mkdir(path)
+		os.chdir(path)
 		print('Downloading to {}\n'.format(path))
 else:
 	print("\nInvalid Response, use either 'y' for 'yes' or 'n' for no\n\n")
 	exit()
 
-inputedLink = input("Input the link you want to download: ")
+inputedLink = input("Input the link download : ")
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([inputedLink])
